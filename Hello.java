b6b3a72fa27ae3d6c7136f140465537a5ace84de
Mandalorian416 <59923777+Mandalorian416@users.noreply.github.com>
2020-01-15 09:53:38 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/*
+Java Program Hello : Lab 1
+
+Brendan Queener	
+1/15/2020
+CMSC 255 003
+*/
+public class Hello
+{
+	public static void main(String[] args)
+	{
+		System.out.println("<Brendan Queener>");
+		System.out.println("<Hello Project>");
+		System.out.println("<Lab 01>");
+		System.out.println("<1/15/2020>");
+		System.out.println("<CMSC 255 Section 003>");
+		System.out.println();
+	}
+}
